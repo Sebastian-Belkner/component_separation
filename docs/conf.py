1199ff8f5bfdf892a2ab3afd86665133b97e2c4f
Sebastian-Belkner <sebastian.belkner@unige.ch>
2021-01-12 14:42:39 +0100

    mock fortran

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ author = 'Sebastian Belkner'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 master_doc = 'index'
+autodoc_mock_imports = ["fortran"]
 
 # -- General configuration ---------------------------------------------------
 
