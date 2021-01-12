ab5284c1f9ea640b3c3a328b361bfbc1ac9bcf3d
Sebastian-Belkner <sebastian.belkner@unige.ch>
2021-01-12 18:02:25 +0100

    split io from rest

---
+++ component_separation/io.py
@@ -0,0 +1,86 @@
+from astropy.io import fits
+from logdecorator import log_on_end, log_on_error, log_on_start
+from logging import DEBUG, ERROR, INFO
+import os
+import sys
+import numpy as np
+from typing import Dict, List, Optional, Tuple
+from component_separation.cs_util import Planckf, Plancks, Planckr
+import healpy as hp
+PLANCKMAPFREQ = [p.value for p in list(Planckf)]
+PLANCKMAPNSIDE = [1024, 2048]
+
+#%% Collect maps
+@log_on_start(INFO, "Starting to grab data without {freqfilter}")
+@log_on_end(DEBUG, "Data without '{freqfilter}' loaded successfully: '{result}' ")
+def get_data(path: str, freqfilter: List[str]) -> List[Dict]:
+    """Collects planck maps (.fits files) and stores to dictionaries. Mask data must be placed in `PATH/mask/`,
+    Map data in `PATH/map/`.
+    Args:
+        path (str): relative path to root dir of the data. Must end with '/'
+        freqfilter (List[str]): Frequency channels which are to be ignored
+
+    Returns:
+        List[Dict]: Planck maps (data and masks) and some header information
+
+    Doctest:
+    >>> get_data(freqfilter=freqfilter) 
+    NotSureWhatToExpect
+    """    
+    mappath = {
+        FREQ:'{path}map/frequency/{LorH}_SkyMap_{freq}-field_{nside}_R3.00_full.fits'
+            .format(
+                path = path,
+                LorH = Planckr.LFI.value if int(FREQ)<100 else Planckr.HFI.value,
+                freq = FREQ,
+                nside = PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1])
+            for FREQ in PLANCKMAPFREQ
+                if FREQ not in freqfilter}
+    tmask = hp.read_map('{}/mask/HFI_Mask_GalPlane-apo0_2048_R2.00.fits'.format(path), field=2, dtype=np.float64)
+    tmask_d = hp.pixelfunc.ud_grade(tmask, nside_out=1024)
+
+    hp_psmask = hp.read_map('{}/mask/psmaskP_2048.fits.gz'.format(path), dtype=np.bool)
+    hp_gmask = hp.read_map('{}/mask/gmaskP_apodized_0_2048.fits.gz'.format(path), dtype=np.bool)
+    pmask = hp_psmask*hp_gmask
+    pmask_d = hp.pixelfunc.ud_grade(pmask, nside_out=1024)
+
+    tmap = {
+        FREQ: {
+            "header": {
+                "nside" : PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1],
+                "freq" : FREQ,
+                "LorH" : Planckr.LFI if int(FREQ)<100 else Planckr.HFI
+            },
+            "map": hp.read_map(mappath[FREQ], field=0),
+            "mask": tmask_d if int(FREQ)<100 else tmask
+            }for FREQ in PLANCKMAPFREQ
+                if FREQ not in freqfilter
+    }
+    
+    qmap = {
+        FREQ: {
+            "header": {
+                "nside" : PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1],
+                "freq" : FREQ,
+                "LorH" : Planckr.LFI if int(FREQ)<100 else Planckr.HFI
+            },
+            "map": hp.read_map(mappath[FREQ], field=1),
+            "mask": pmask_d if int(FREQ)<100 else pmask
+            }for FREQ in PLANCKMAPFREQ
+                if FREQ not in freqfilter
+    }
+
+    umap = {
+        FREQ: {
+            "header": {
+                "nside" : PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1],
+                "freq" : FREQ,
+                "LorH" : Planckr.LFI if int(FREQ)<100 else Planckr.HFI
+            },
+            "map": hp.read_map(mappath[FREQ], field=2),
+            "mask": pmask_d if int(FREQ)<100 else pmask
+            }for FREQ in PLANCKMAPFREQ
+                if FREQ not in freqfilter
+    }
+    return [tmap, qmap, umap]
+# %%
--- component_separation/powspec.py
+++ component_separation/powspec.py
@@ -1,3 +1,6 @@
+# pixel window function needs to be taken into account,  see Healpy.pixwin()
+
+
 """
 The ``powspec`` module
 ======================
@@ -25,8 +28,7 @@ in your command line.
     # >>> cov = build_covmatrices(df_scbf, lmax, freqfilter, specfilter)
     # >>> cov_inv_l = invert_covmatrices(cov, lmax, freqfilter, specfilter)
     # >>> weights = calculate_weights(cov_inv_l, lmax, freqfilter, specfilter)
-    >>> 1
-    1
+
 
 """
 
@@ -37,106 +39,33 @@ import sys
 from logging import DEBUG, ERROR, INFO
 from typing import Dict, List, Optional, Tuple
 
-import healpy as hp
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
-from astropy.io import fits
 from logdecorator import log_on_end, log_on_error, log_on_start
 
-import component_separation.pospace as ps #TODO change to MSC.pospace once submodule issue is fixed
-
-from component_separation.cs_util import Planckf
+import component_separation.pospace as ps
+from component_separation.cs_util import Planckf, Plancks, Planckr
 
 PLANCKMAPFREQ = [p.value for p in list(Planckf)]
-PLANCKSPECTRUM = ["TT", "EE", "BB", "TE", "TB", "EB", "ET", "BT", "BE"]
-PLANCKMAPAR = ["LFI", "HFI"]
+PLANCKSPECTRUM = [p.value for p in list(Plancks)]
+PLANCKMAPAR = [p.value for p in list(Planckr)]
 PLANCKMAPNSIDE = [1024, 2048]
 
 
+""" Doctest:
+The following constants be needed because functions are called with globals()
+"""
 freqfilter = [Planckf.LFI_1.value, Planckf.LFI_2.value, Planckf.HFI_1.value, Planckf.HFI_5.value, Planckf.HFI_6.value]
-specfilter = ["TE", "TB", "ET", "BT"]
+specfilter = [Plancks.TE, Plancks.TB, Plancks.ET, Plancks.BT]
 lmax = 20
 lmax_mask = 80
 
 
 def set_logger(loglevel=logging.INFO):
     logging.basicConfig(format='   %(levelname)s:      %(message)s', level=loglevel)
-
-#%% Collect maps
-@log_on_start(INFO, "Starting to grab data without {freqfilter}")
-@log_on_end(DEBUG, "Data without '{freqfilter}' loaded successfully: '{result}' ")
-def get_data(path: str, freqfilter: List[str]) -> List[Dict]:
-    """Collects planck maps (.fits files) and stores to dictionaries. Mask data must be placed in `PATH/mask/`,
-    Map data in `PATH/map/`.
-    Args:
-        path (str): relative path to root dir of the data. Must end with '/'
-        freqfilter (List[str]): Frequency channels which are to be ignored
-
-    Returns:
-        List[Dict]: Planck maps (data and masks) and some header information
-
-    Doctest:
-    >>> get_data(freqfilter=freqfilter) 
-    NotSureWhatToExpect
-    """    
-    mappath = {
-        FREQ:'{path}map/frequency/{LorH}_SkyMap_{freq}-field_{nside}_R3.00_full.fits'
-            .format(
-                path = path,
-                LorH = PLANCKMAPAR[0] if int(FREQ)<100 else PLANCKMAPAR[1],
-                freq = FREQ,
-                nside = PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1])
-            for FREQ in PLANCKMAPFREQ
-                if FREQ not in freqfilter}
-    tmask = hp.read_map('{}/mask/HFI_Mask_GalPlane-apo0_2048_R2.00.fits'.format(path), field=2, dtype=np.float64)
-    tmask_d = hp.pixelfunc.ud_grade(tmask, nside_out=1024)
-
-    hp_psmask = hp.read_map('{}/mask/psmaskP_2048.fits.gz'.format(path), dtype=np.bool)
-    hp_gmask = hp.read_map('{}/mask/gmaskP_apodized_0_2048.fits.gz'.format(path), dtype=np.bool)
-    pmask = hp_psmask*hp_gmask
-    pmask_d = hp.pixelfunc.ud_grade(pmask, nside_out=1024)
-
-    tmap = {
-        FREQ: {
-            "header": {
-                "nside" : PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1],
-                "freq" : FREQ,
-                "LorH" : PLANCKMAPAR[0] if int(FREQ)<100 else PLANCKMAPAR[1]
-            },
-            "map": hp.read_map(mappath[FREQ], field=0),
-            "mask": tmask_d if int(FREQ)<100 else tmask
-            }for FREQ in PLANCKMAPFREQ
-                if FREQ not in freqfilter
-    }
-
-    qmap = {
-        FREQ: {
-            "header": {
-                "nside" : PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1],
-                "freq" : FREQ,
-                "LorH" : PLANCKMAPAR[0] if int(FREQ)<100 else PLANCKMAPAR[1]
-            },
-            "map": hp.read_map(mappath[FREQ], field=1),
-            "mask": pmask_d if int(FREQ)<100 else pmask
-            }for FREQ in PLANCKMAPFREQ
-                if FREQ not in freqfilter
-    }
-
-    umap = {
-        FREQ: {
-            "header": {
-                "nside" : PLANCKMAPNSIDE[0] if int(FREQ)<100 else PLANCKMAPNSIDE[1],
-                "freq" : FREQ,
-                "LorH" : PLANCKMAPAR[0] if int(FREQ)<100 else PLANCKMAPAR[1]
-            },
-            "map": hp.read_map(mappath[FREQ], field=2),
-            "mask": pmask_d if int(FREQ)<100 else pmask
-            }for FREQ in PLANCKMAPFREQ
-                if FREQ not in freqfilter
-    }
-    return [tmap, qmap, umap]
 
 #%% Calculate spectrum
 @log_on_start(INFO, "Starting to calculate powerspectra up to lmax={lmax} and lmax_mask={lmax_mask}")
@@ -407,7 +336,7 @@ def general_pipeline():
 
     set_logger()
     
-    tqumap = get_data(freqfilter=freqfilter)
+    tqumap = get_data(path='data/', freqfilter=freqfilter)
     spectrum = powerspectrum(tqumap, lmax, lmax_mask, freqfilter, specfilter)
     df = create_df(spectrum, freqfilter, specfilter)
     df_sc = apply_scale(df, specfilter)
